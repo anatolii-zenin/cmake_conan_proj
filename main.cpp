83824b08cb9502d574e5efae7e60fcea2d69b463
Anatolii Zenin <anatolii.zenin@gmail.com>
2023-06-06 21:15:01 +0200

    add glfw window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <GLFW/glfw3.h>
 
 int main(int argc, char** argv)
 {
     std::cout << "this is working" << std::endl;
+
+    GLFWwindow *window;
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+    while( !glfwWindowShouldClose(window) )
+    {
+        // Draw gears
+        //draw();
+
+        // Update animation
+        //animate();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
     return 0;
 }
